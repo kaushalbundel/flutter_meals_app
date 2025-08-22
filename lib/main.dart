43b0d8e559ec8f39daaae153e3f99d2080d2aa96
Kaushal Bundel <kaushalbundel@gmail.com>
2025-08-22 19:09:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// setting broad theme
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', theme: theme, home: ,);
+  }
+}
